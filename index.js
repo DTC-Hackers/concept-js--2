63896b697f1676941acd7068a3ec4ebe17181569
jdidi94 <jdididaoud404@gmail.com>
2022-03-03 18:15:55 +0100

    it's ready

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function findMax(x, y) {}
 // Output: 99.00
 // Transaction unsuccessful
 function checkBalance(x, bal) {}
+
 // exercise 2
 
 // In the game of golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
@@ -37,12 +38,13 @@ const names = [
   "Go Home!",
 ];
 // Strokes       Return
+
 // 1	          "Hole-in-one!"
 // <= par - 2	  "Eagle"
-// par - 1	      "Birdie"
-// par	          "Par"
-// par + 1	      "Bogey"
-// par + 2	      "Double Bogey"
+// par - 1	    "Birdie"
+// par	        "Par"
+// par + 1	    "Bogey"
+// par + 2	    "Double Bogey"
 // >= par + 3	  "Go Home!"
 
 // golfScore(5, 4);
